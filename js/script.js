33e6ebb5ba43ec00dc1c4157062944d5a276c7da
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2023-04-25 13:31:07 -0300

    Posição scroll-animado no script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 import initAccordionList from "./modules/accordion-list.js";
 import initTabNavegacao from "./modules/tab-navegacao.js";
 import ScrollSuave from "./modules/scroll-suave.js";
-import initScrollAnimado from "./modules/scroll-animado.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -9,12 +8,12 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initHorarioSemana from "./modules/horario-semana.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initScrollAnimado from "./modules/scroll-animado.js";
 
 initTabNavegacao();
 initAccordionList();
 const scrollSuave = new ScrollSuave('[data-scroll="suave"] a[href^="#"]');
 scrollSuave.init();
-initScrollAnimado();
 initModal();
 initTooltip();
 initDropdownMenu();
@@ -22,3 +21,4 @@ initMenuMobile();
 initHorarioSemana();
 initFetchAnimais();
 initFetchBitcoin();
+initScrollAnimado();
